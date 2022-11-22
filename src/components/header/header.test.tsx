03e93deada01d4b2aa5a8aff7596bc9ec09d208b
Felipe <v.uliana@gmail.com>
2022-11-22 19:46:05 -0300

    chore(component): add 'Header' specs

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Header from '.'
+
+describe('Header component', () => {
+  it.todo('should display the heading')
+
+  it.todo('should display the subtitle')
+
+  it.todo('should display the description')
+})
---
+++ src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FC } from 'react'
+
+const Header: FC = () => (
+  <header>
+    <h1>Header</h1>
+  </header>
+)
+
+export default Header
