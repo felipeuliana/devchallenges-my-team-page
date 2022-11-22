81e62a789c587b980444efc2856ca1104be1ece3
Felipe <v.uliana@gmail.com>
2022-11-22 20:18:11 -0300

    feat(component): add 'Header' story

---
+++ src/components/header/header.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Header from '.'
+
+export default {
+  args: {
+    description: 'We are team of creatively diverse.  driven.  innovative individuals working in various loctions from the world.',
+    heading: 'The creative crew',
+    subtitle: 'Who we are',
+  },
+  component: Header,
+  title: 'Header',
+} as ComponentMeta<typeof Header>
+
+export const Default: ComponentStory<typeof Header> = (args) => (
+  <Header {...args} />
+)
