967e806e4d3339ca42e1c3cfd4c90f3bb8192ea7
Felipe <v.uliana@gmail.com>
2022-11-25 19:22:40 -0300

    feat(component): add 'Card' story

---
+++ src/components/card/card.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Card from './card'
+
+const CardMeta: ComponentMeta<typeof Card> = {
+  args: {
+    image: {
+      alt: 'Woman avatar image.',
+      src: 'michael-dam-mEZ3PoFGs_k-unsplash.jpg'
+    },
+    name: 'Saba Cabrera',
+    title: 'Art director'
+  },
+  component: Card,
+  title: 'Card'
+}
+
+export const Default: ComponentStory<typeof Card> = (args) => (
+  <Card {...args} />
+)
+
+export default CardMeta
