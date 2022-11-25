82dde4333a78dca92dbb28bb6a69d128b6e2ecf2
Felipe <v.uliana@gmail.com>
2022-11-25 18:35:27 -0300

    chore(component): add 'Card' specs

---
+++ src/components/card/card.test.tsx
@@ -0,0 +1,7 @@
+describe('Team member card', () => {
+  it.todo('should display the team member image')
+
+  it.todo('should display the team member title')
+
+  it.todo('should display the team member name')
+})
---
+++ src/components/card/card.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+const Card: FC = () => (
+  <div>
+    <h1>Card team member</h1>
+  </div>
+)
+
+export default Card
