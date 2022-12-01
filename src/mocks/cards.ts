138be24f99a50f95c2bef9a78008aabafcd6f051
Felipe <v.uliana@gmail.com>
2022-12-01 13:45:56 -0300

    chore(mock): add cards data

---
+++ src/mocks/cards.ts
@@ -0,0 +1,50 @@
+export const CARDS = [
+  {
+    image: {
+      alt: 'Avatar of Terry Medhurst',
+      src: 'https://robohash.org/hicveldicta.png'
+    },
+    name: 'Terry Medhurst',
+    title: 'Product owner'
+  },
+  {
+    image: {
+      alt: 'Avatar of Sheldon Quigley',
+      src: 'https://robohash.org/doloremquesintcorrupti.png'
+    },
+    name: 'Sheldon Quigley',
+    title: 'Art director'
+  },
+  {
+    image: {
+      alt: 'Avatar of Terrill Hills',
+      src: 'https://robohash.org/consequunturautconsequatur.png'
+    },
+    name: 'Terrill Hills',
+    title: 'Tech Lead'
+  },
+  {
+    image: {
+      alt: 'Avatar of Miles Cummerata',
+      src: 'https://robohash.org/facilisdignissimosdolore.png'
+    },
+    name: 'Miles Cummerata',
+    title: 'UX Designer'
+  },
+  {
+    image: {
+      alt: 'Avatar of Mavis Schultz',
+      src: 'https://robohash.org/adverovelit.png'
+    },
+    name: 'Mavis Schultz',
+    title: 'Developer'
+  },
+  {
+    image: {
+      alt: 'Avatar of Alison Reichert',
+      src: 'https://robohash.org/laboriosamfacilisrem.png'
+    },
+    name: 'Alison Reichert',
+    title: 'Developer'
+  }
+]
