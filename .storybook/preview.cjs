ca7246c43949310a09539cca43d6fb3b9bb70d28
Felipe <v.uliana@gmail.com>
2022-12-01 13:50:59 -0300

    chore(config): customize Storybook viewports

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -6,4 +6,29 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: {
+      mobile: {
+        name: 'mobile',
+        styles: {
+          height: '375px',
+          width: '667px',
+        }
+      },
+      tablet: {
+        name: 'tablet',
+        styles: {
+          height: '1024px',
+          width: '768px',
+        }
+      },
+      desktop: {
+        name: 'desktop',
+        styles: {
+          height: '1366px',
+          width: '768px',
+        }
+      }
+    }
+  }
 }
